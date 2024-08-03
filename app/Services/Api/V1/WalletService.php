cc7eba9f2c7175c8044f870efab5cf71a4bc9af6
dakeem05 <edidiongsamuel14@gmail.com>
2024-08-03 17:15:47 +0100

    Changed teh paginate to test

--- app/Services/Api/V1/WalletService.php
+++ app/Services/Api/V1/WalletService.php
@@ -204,7 +204,7 @@ class WalletService
 
     public function getTransactions (Int $user_id)
     {
-        $transactions = Transaction::where('user_id', $user_id)->latest()->paginate();
+        $transactions = Transaction::where('user_id', $user_id)->latest()->paginate(9);
         return $transactions;
     }
 
